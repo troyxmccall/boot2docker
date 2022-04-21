f990bdc84b740a138d36863d278b88e7a5eb41fc
Troy McCall <troyxmccall@gmail.com>
2022-04-21 17:25:32 -0400

    update build script

--- build.sh
+++ build.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-docker build -t boot2docker/boot2docker:base base/
-docker build -t boot2docker/boot2docker-rootfs rootfs/
-docker rm build-boot2docker
-# you will need more than 2GB memory for the next step
-docker run --name build-boot2docker boot2docker/boot2docker-rootfs
-docker cp build-boot2docker:/boot2docker.iso
+docker build -t boot2docker . && docker run —rm boot2docker > boot2docker.iso
