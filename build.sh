e4ab7ff2f25c72145e3529d8b759f7589f08f790
Troy McCall <troyxmccall@gmail.com>
2024-04-29 13:25:13 -0400

    bump kernel

--- build.sh
+++ build.sh
@@ -7,8 +7,6 @@ if [ $? -ne 0 ]; then
 fi
 
 docker run --rm boot2docker > boot2docker.iso
-#make a backup of the final v19.03.12 release just in case
-wget --output-document boot2docker-v19.03.12.iso https://github.com/boot2docker/boot2docker/releases/download/v19.03.12/boot2docker.iso
 # stop our machine
 docker-machine stop default
 # copy our new iso
